5586d733f99376191c9407c855821985952550e8
Alexander Novy <novy.alexander@gmail.com>
2021-05-27 13:36:01 +0000

    Add description to landing page in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
 def scale(payload):
-    """Scales Payload"""
+    """Scales Payload """
 
     LOG.info("Scaling Payload: %s payload")
     scaler = StandardScaler().fit(payload)
